c71d9708f75fc411ff4907cbbe975e809b0534d0
Thomas Glenn <thomasgrantglenn@mac.com>
2020-12-08 15:46:03 -0700

    add graph tests and methods.

---
+++ __tests__/graph.test.js
@@ -0,0 +1,61 @@
+import Graph from '../src/graph.js';
+
+describe('Graph', () => {
+
+  let graph = new Graph();
+
+  afterEach(() => {
+    graph = new Graph();
+  });
+
+  test('should correctly instantiate a graph', () => {
+    expect(graph.adjacencyList.size).toEqual(0);
+  });
+
+  test('should add a new node', () => {
+    graph.addNode("Jasmine");
+    expect(graph.adjacencyList.get("Jasmine").size).toEqual(0);
+  });
+
+  test('should return null if the node does not exist in the adjacency list', () => {
+    expect(graph.hasNode("Ada")).toEqual(false);
+  });
+
+  test('check to see if node exists in graph', () => {
+    graph.addNode("Jasmine");
+    expect(graph.hasNode("Jasmine")).toEqual(true);
+  });
+
+  test('add an edge between two nodes', () => {
+    graph.addNode("Jasmine");
+    graph.addNode("Ada");
+    graph.createEdge("Jasmine", "Ada");
+    expect(graph.hasEdge("Ada", "Jasmine")).toEqual(true);
+  });
+
+  test('check to see if edge exists in graph', () => {
+    graph.addNode("Jasmine");
+    graph.addNode("Ada");
+    expect(graph.hasEdge("Jasmine", "Ada")).toEqual(false);
+  });
+
+  test('remove an edge between two nodes', () => {
+    graph.addNode("Jasmine");
+    graph.addNode("Ada");
+    graph.createEdge("Jasmine", "Ada");
+    graph.removeEdge("Jasmine", "Ada");
+    expect(graph.hasEdge("Ada", "Jasmine")).toEqual(false);
+  });
+
+  test('should delete a node and all of its adjacent nodes', () => {
+    graph.addNode("Ada");
+    graph.addNode("Jasmine");
+    graph.addNode("Lydia");
+    graph.createEdge("Ada", "Jasmine");
+    graph.createEdge("Ada", "Lydia");
+    graph.removeNode("Ada");
+    expect(graph.hasNode("Ada")).toEqual(false);
+    expect(graph.hasEdge("Jasmine", "Ada")).toEqual(false);
+    expect(graph.hasEdge("Lydia", "Ada")).toEqual(false);
+  });
+});
---
+++ src/graph.js
@@ -0,0 +1,46 @@
+export default class Graph {
+  constructor() {
+    this.adjacencyList = new Map();
+  }
+
+  addNode(name) {
+    this.adjacencyList.set(name, new Set());
+  }
+
+  hasNode(name) {
+    return false;
+  }
+
+  hasNode(name) {
+    if (this.adjacencyList.get(name)) {
+      return true;
+    }
+    return false;
+  }
+
+  createEdge(node1, node2) {
+    this.adjacencyList.get(node1).add(node2);
+    this.adjacencyList.get(node2).add(node1);
+  }
+
+  hasEdge(node1, node2) {
+    if (this.adjacencyList.get(node1).has(node2)) {
+      return true
+    }
+    return false;
+  }
+
+  removeEdge(node1, node2) {
+    this.adjacencyList.get(node1).delete(node2);
+    this.adjacencyList.get(node2).delete(node1);
+  }
+
+  removeNode(name) {
+    if (this.adjacencyList.has(name)) {
+      this.adjacencyList.get(name).forEach((edge) => {
+        this.adjacencyList.get(edge).delete(name);
+      });
+      this.adjacencyList.delete(name);
+    }
+  }
+}
